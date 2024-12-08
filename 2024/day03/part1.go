0288aac05490a8969fad2645212f121a883228a3
Matthew Skinner <matthew@skinnerdev.com>
2024-12-08 11:23:04 -0600

    part 1 day 3

--- 2024/day03/part1.go
+++ 2024/day03/part1.go
@@ -1,17 +1,42 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 )
 
 func main() {
-	// Read the file
-	file, err := os.Open("input.txt")
+	input, err := os.Open("input.txt")
 	if err != nil {
-		fmt.Println("Error opening file:", err)
+		fmt.Println("Error opening input:", err)
 		return
 	}
-	defer file.Close()
+	defer input.Close()
 
+	scanner := bufio.NewScanner(input)
+
+	re := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+
+	total := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		matches := re.FindAllStringSubmatch(line, -1)
+
+		for _, match := range matches {
+			x, _ := strconv.Atoi(match[1])
+			y, _ := strconv.Atoi(match[2])
+			total += x * y
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	}
+
+	fmt.Printf("Sum of all multiplication results: %d\n", total)
 }
